2fe6a781977631a4b591638aa121c0fa2c575083
azimut <azimut.github@protonmail.com>
2021-09-25 02:09:08 -0300

    * internal/hackernews/printer.go:

--- internal/hackernews/printer.go
+++ internal/hackernews/printer.go
@@ -2,9 +2,9 @@ package hackernews
 
 import (
 	"fmt"
+	"math"
 	"strings"
 
-	text "github.com/MichaelMure/go-term-text"
 	"github.com/PuerkitoBio/goquery"
 	"github.com/dustin/go-humanize"
 	"jaytaylor.com/html2text"
@@ -35,7 +35,7 @@ func (c *Comment) String() (ret string) {
 	if err != nil {
 		panic(err)
 	}
-	wrapped, _ := text.WrapLeftPadded(msg, 120, indent+1)
+	wrapped := leftPad(msg, 120, indent+1)
 	ret += "\n" + wrapped + "\n"
 	arrow := ">>> "
 	if c.indent > 0 {
@@ -43,4 +43,29 @@ func (c *Comment) String() (ret string) {
 	}
 	ret += strings.Repeat(" ", indent) + arrow + c.user + " - " + humanize.Time(c.date)
 	return
+}
+
+func leftPad(text string, maxWidth int, padding int) string {
+	lines := splitByWidthMake(text, maxWidth-padding)
+	pad := strings.Repeat(" ", padding)
+	for i, line := range lines {
+		lines[i] = pad + line
+	}
+	return strings.Join(lines, "\n")
+}
+
+func splitByWidthMake(str string, size int) []string {
+	strLength := len(str)
+	splitedLength := int(math.Ceil(float64(strLength) / float64(size)))
+	splited := make([]string, splitedLength)
+	var start, stop int
+	for i := 0; i < splitedLength; i += 1 {
+		start = i * size
+		stop = start + size
+		if stop > strLength {
+			stop = strLength
+		}
+		splited[i] = str[start:stop]
+	}
+	return splited
 }
