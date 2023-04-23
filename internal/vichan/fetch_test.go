25407e0ed4708eb21dbb2b3c7dba9c486ed42bc8
azimut <azimut.github@protonmail.com>
2023-04-23 14:48:21 -0300

    vichan: fix fetch_test.go

--- internal/vichan/fetch_test.go
+++ internal/vichan/fetch_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestFetch(t *testing.T) {
 	url := "https://lainchan.org/sec/res/18084.html"
-	thread, err := Fetch(url, "LainView/1.0", time.Second*5)
+	thread, err := Fetch(url, "LainView/1.0", 100, 3, time.Second*5)
 	if err != nil {
 		t.Errorf("%v", err)
 	}
