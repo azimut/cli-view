685fe88731ac1d581ea9b704ce862fadc85f6c06
azimut <azimut.github@protonmail.com>
2023-04-26 17:02:54 -0300

    fix hackernews fetch_test.go

--- internal/hackernews/fetch_test.go
+++ internal/hackernews/fetch_test.go
@@ -8,7 +8,7 @@ import (
 const URL = `https://news.ycombinator.com/item?id=3078128`
 
 func TestHNFetch(t *testing.T) {
-	_, _, err := Fetch(URL, time.Second*10, 10, 2)
+	_, err := Fetch(URL, time.Second*10, 10, 2)
 	if err != nil {
 		t.Fail()
 	}
