358c0a2884f88842659b0db603d8b52c2128ba2c
azimut <azimut.github@protonmail.com>
2023-04-26 19:39:45 -0300

    fix hackernews fetch

--- internal/reddit/fetch_test.go
+++ internal/reddit/fetch_test.go
@@ -8,7 +8,7 @@ import (
 const URL = `https://old.reddit.com/r/gamedev/comments/horm1a/ive_started_making_a_teaching_aid_for_people/`
 
 func TestRTFetch(t *testing.T) {
-	_, err := Fetch(URL, "Reddit_Cli/0.1", 100, 3, time.Second*10)
+	_, err := Fetch(URL, "Reddit_Cli/0.1", time.Second*10)
 	if err != nil {
 		t.Fail()
 	}
