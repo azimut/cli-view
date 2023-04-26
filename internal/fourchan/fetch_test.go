9c8d0314cdbd67c5a4a8893095c9fc76c7bea0c6
azimut <azimut.github@protonmail.com>
2023-04-26 09:16:58 -0300

    fix fetch_test.go

--- internal/fourchan/fetch_test.go
+++ internal/fourchan/fetch_test.go
@@ -8,7 +8,7 @@ import (
 const URL = `https://boards.4channel.org/g/thread/76759434/this-board-is-for-the-discussion-of-technology`
 
 func TestFormatThread(t *testing.T) {
-	thread, err := Fetch(URL)
+	thread, err := Fetch(URL, 100, 3)
 	if err != nil {
 		t.FailNow()
 	}
