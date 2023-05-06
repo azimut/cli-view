db8352530a9d1f625359da7484723636f25d69c5
azimut <azimut.github@protonmail.com>
2023-05-05 21:12:19 -0300

    twitter: clean tweet link

--- internal/twitter/validate.go
+++ internal/twitter/validate.go
@@ -3,6 +3,7 @@ package twitter
 import (
 	"errors"
 	"net/url"
+	"regexp"
 )
 
 func EffectiveUrl(rawUrl string) (string, error) {
@@ -18,5 +19,7 @@ func EffectiveUrl(rawUrl string) (string, error) {
 	}
 	uri.RawQuery = ""
 	uri.Scheme = "https"
+	reTrimmer := regexp.MustCompile("/photo/[0-9]+$|/video/[0-9]+$|/retweets/with_comments$")
+	uri.Path = reTrimmer.ReplaceAllString(uri.Path, "")
 	return "https://publish.twitter.com/oembed?url=" + uri.String(), nil
 }
