7e94697ca7171d6a0ab6adaca590c9bffcdef770
azimut <azimut.github@protonmail.com>
2023-09-05 03:36:25 -0300

    discourse input url validation, fixes #17

--- internal/discourse/validate.go
+++ internal/discourse/validate.go
@@ -1,31 +1,16 @@
 package discourse
 
 import (
-	"errors"
-	"net/url"
-	"strconv"
-	"strings"
+	"fmt"
+	"regexp"
 )
 
+var urlRegex = regexp.MustCompile(`http[s]?://[^/]+/t/[^/]+/[0-9]+`)
+
 func effectiveUrl(rawUrl string) (string, error) {
-	url, err := url.Parse(rawUrl)
-	if err != nil {
-		return "", err
+	url := urlRegex.FindString(rawUrl)
+	if url == "" {
+		return "", fmt.Errorf("invalid url: %s", rawUrl)
 	}
-
-	paths := strings.Split(url.Path, "/")
-	if len(paths) != 4 {
-		return "", errors.New("invalid url, does not enough paths")
-	}
-
-	if paths[1] != "t" {
-		return "", errors.New("invalid url, does not have a `/t/` path")
-	}
-
-	rawId := paths[3]
-	if _, err = strconv.Atoi(rawId); err != nil {
-		return "", errors.New("invalid url, does not end with a number")
-	}
-
-	return url.String() + ".json", nil
+	return url + ".json", nil
 }
--- internal/discourse/validate_test.go
+++ internal/discourse/validate_test.go
@@ -4,8 +4,10 @@ import "testing"
 
 func TestEffectiveUrl(t *testing.T) {
 	testUrls := map[string]string{
-		"https://llllllll.co/t/disquiet-junto-project-0590-concrete-roots/62027": "https://llllllll.co/t/disquiet-junto-project-0590-concrete-roots/62027.json",
-		"https://0x00sec.org/t/question-on-your-need-to-be-known/34563":          "https://0x00sec.org/t/question-on-your-need-to-be-known/34563.json",
+		"https://llllllll.co/t/disquiet-junto-project-0590-concrete-roots/62027":       "https://llllllll.co/t/disquiet-junto-project-0590-concrete-roots/62027.json",
+		"https://0x00sec.org/t/question-on-your-need-to-be-known/34563":                "https://0x00sec.org/t/question-on-your-need-to-be-known/34563.json",
+		"https://discourse.haskell.org/t/an-opportunity-that-i-couldnt-pass-up/7485/":  "https://discourse.haskell.org/t/an-opportunity-that-i-couldnt-pass-up/7485.json",
+		"https://discourse.haskell.org/t/an-opportunity-that-i-couldnt-pass-up/7485/3": "https://discourse.haskell.org/t/an-opportunity-that-i-couldnt-pass-up/7485.json",
 	}
 	for rawUrl, expected := range testUrls {
 		got, _ := effectiveUrl(rawUrl)
